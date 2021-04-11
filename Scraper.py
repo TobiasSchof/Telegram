85e34996f47433deb9c157eccede7340f735ec33
Tobias <tobiasms@caltech.edu>
2021-04-11 14:35:37 -0700

    Test file with example of API usage

---
+++ Scraper.py
@@ -0,0 +1,8 @@
+from astropy.io import fits
+from telethon import TelegramClient
+from datetime import datetime
+from configparser import ConfigParser
+import telethon.sync # this relieves the need to use telethon as an asyncio library
+
+def load_range(channel:str, start:datetime, end:datetime, data_dir:str):
+    """loads and files 
