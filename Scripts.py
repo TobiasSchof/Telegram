702874f00061a74e11a1e4e2c1a5b8f4599fdf15
Tobias <tobiasms@caltech.edu>
2021-08-01 12:51:52 -0700

    added script to plot data from database

--- Scripts.py
+++ Scripts.py
@@ -4,6 +4,7 @@ from configparser import ConfigParser
 import os, sys
 import sqlite3
 import numpy as np
+import matplotlib.pyplot as plt
 
 working_dir = os.path.join(os.path.expanduser("~"), "Documents", "Thesis")
 
@@ -12,6 +13,55 @@ cf = ConfigParser()
 _ = cf.read(os.path.join(working_dir, ".Telegram_info.ini"))
 if len(_) == 0:
     raise FileNotFoundError("No telegram info file found.")
+
+def plot():
+    """A function to plot things from the database"""
+
+    # make this point to the database you want
+    with sqlite3.connect("/Users/gretathesis/Desktop/Thesis.db") as db:
+        # fill in what tags, etc you want to graph here
+        set_1 = db.execute("SELECT * FROM Scraper WHERE (`CHANNEL` = 'nexta_tv' OR `CHANNEL` = 'nexta_live') AND NOT (`XPost` = 't.me/nexta_tv' OR `XPost` = 't.me/nexta_live' OR `XPost` = 't.me/luxta_tv') AND (`1.3 Elderly` = 1 OR `3.3.1 Traditional Base` = 1)").fetchall()
+        set_2 = db.execute("SELECT * FROM Scraper WHERE (`CHANNEL` = 'nexta_tv' OR `CHANNEL` = 'nexta_live') AND NOT (`XPost` = 't.me/nexta_tv' OR `XPost` = 't.me/nexta_live' OR `XPost` = 't.me/luxta_tv') AND (`13. WWII` = 1)").fetchall()
+
+    # get days for all the messages pulled
+    set_1_days = [post[4][:10] for post in set_1]
+    set_2_days = [post[4][:10] for post in set_2]
+
+    # get unique days
+    set_1_days_u = []
+    for date in set_1_days:
+        if date not in set_1_days_u: set_1_days_u.append(date)
+    set_2_days_u = []
+    for date in set_2_days:
+        if date not in set_2_days_u: set_2_days_u.append(date)
+
+    # sort days
+    set_1_days_u.sort()
+    set_2_days_u.sort()
+
+    # get number of posts per day
+    set_1_cnts = [set_1_days.count(date) for date in set_1_days_u]
+    set_2_cnts = [set_2_days.count(date) for date in set_2_days_u]
+
+    # get figure, axis so we can modify appearance
+    fig, ax = plt.subplots()
+
+    # plot data
+    ax.plot(set_1_days_u, set_1_cnts)
+    ax.plot(set_2_days_u, set_2_cnts)
+
+    # get x tick labels (every 14 days)
+    start = datetime(year = 2020, month = 8, day = 1)
+    end = datetime(year = 2020, month = 12, day = 1)
+    ticks = [start]
+    while ticks[-1] + timedelta(days = 14) < end:
+        ticks.append(ticks[-1] + timedelta(days = 14))
+
+    # change ticks to YYYY-MM-DD
+    ticks = [date.isoformat()[:10] for date in ticks]
+
+    # plot figure
+    fig.show()
 
 def avg_win(chnl, start_id, end_id, type_):
     """Return mean, median, and std of views in id range for channel
