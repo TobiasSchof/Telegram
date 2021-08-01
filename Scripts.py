98924aaae861af5e828e962d6bafd034dfd76e55
Tobias - Greta's spare laptop <toby.s117@gmail.com>
2021-08-01 12:38:52 -0700

    function for getting an average view count in an id window

--- Scripts.py
+++ Scripts.py
@@ -2,6 +2,8 @@ from Scraper import Scraper
 from datetime import datetime, timedelta, timezone
 from configparser import ConfigParser
 import os, sys
+import sqlite3
+import numpy as np
 
 working_dir = os.path.join(os.path.expanduser("~"), "Documents", "Thesis")
 
@@ -10,6 +12,35 @@ cf = ConfigParser()
 _ = cf.read(os.path.join(working_dir, ".Telegram_info.ini"))
 if len(_) == 0:
     raise FileNotFoundError("No telegram info file found.")
+
+def avg_win(chnl, start_id, end_id, type_):
+    """Return mean, median, and std of views in id range for channel
+
+    Args:
+        chnl = the channel to look at
+        start_id = id to start range (inclusive)
+        end_id = the id to end range (inclusive)
+        type = one of "views" or "forwards"
+    Returns:
+        float, float, float = mean, standard deviation, median
+    """
+
+    if not type_.lower() in ["views", "forwards"]:
+        raise ValueError("type_ must be either views or forwards, not {}".format(type_))
+
+    with sqlite3.connect("/Users/gretathesis/Desktop/Thesis.db") as db:
+        msgs = db.execute("SELECT `{}` FROM Scraper WHERE `CHANNEL` = ? AND `ID` BETWEEN ? AND ?".format(type_), (chnl, start_id, end_id)).fetchall()
+
+    if len(msgs) == 0:
+        raise ValueError("No info found for channel {} between id {} and id {}".format(chnl, start_id, end_id))
+
+    nums = np.array([msg[0] for msg in msgs])
+    mean = np.mean(nums, 0)
+    std = np.std(nums, 0)
+    med = np.median(nums, 0)
+    print("{} for range: {}".format(type_, nums))
+    print("mean: {}, std: {}, median: {}".format(mean, std, med))
+    return mean, std, med
 
 def download_chnl(chnl, start, end, db=os.path.join(working_dir, "Thesis.db"), verbose=True):
     """Download all messages and media to the given database for the given channel
