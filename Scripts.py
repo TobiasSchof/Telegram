7d64a759f33a6fe52987248353bc5d9aa35e4675
Tobias - Greta's spare laptop <toby.s117@gmail.com>
2021-08-12 19:55:40 -0400

    worked on graphs, not sure if real changes or just different data

--- Scripts.py
+++ Scripts.py
@@ -21,12 +21,26 @@ def plot():
     # make this point to the database you want
     with sqlite3.connect("/Users/gretathesis/Desktop/Thesis.db") as db:
         # fill in what tags, etc you want to graph here
-        set_1 = db.execute("SELECT * FROM Scraper WHERE (`CHANNEL` = 'nexta_tv' OR `CHANNEL` = 'nexta_live') AND NOT (`XPost` = 't.me/nexta_tv' OR `XPost` = 't.me/nexta_live' OR `XPost` = 't.me/luxta_tv') AND (`1.3 Elderly` = 1 OR `3.3.1 Traditional Base` = 1)").fetchall()
-        set_2 = db.execute("SELECT * FROM Scraper WHERE (`CHANNEL` = 'nexta_tv' OR `CHANNEL` = 'nexta_live') AND NOT (`XPost` = 't.me/nexta_tv' OR `XPost` = 't.me/nexta_live' OR `XPost` = 't.me/luxta_tv') AND (`13. WWII` = 1)").fetchall()
+        set_1 = db.execute("SELECT * FROM Scraper WHERE (`CHANNEL` = 'nexta_tv' OR `CHANNEL` = 'nexta_live') AND NOT (`XPost` = 't.me/nexta_tv' OR `XPost` = 't.me/nexta_live' OR `XPost` = 't.me/luxta_tv') AND (`4.6 Fighting Back` = 1 OR `14.1 Encourage Violence` = 1)").fetchall()
+        set_2 = db.execute("SELECT * FROM Scraper WHERE (`CHANNEL` = 'nexta_tv' OR `CHANNEL` = 'nexta_live') AND NOT (`XPost` = 't.me/nexta_tv' OR `XPost` = 't.me/nexta_live' OR `XPost` = 't.me/luxta_tv') AND (`5.9 Dehumanization` = 1)").fetchall()
+        #set_3 = db.execute("SELECT * FROM Scraper WHERE (`CHANNEL` = 'nexta_tv' OR `CHANNEL` = 'nexta_live') AND NOT (`XPost` = 't.me/nexta_tv' OR `XPost` = 't.me/nexta_live' OR `XPost` = 't.me/luxta_tv') AND (`5.4 Incompetence` = 1)").fetchall()
+        #set_4 = db.execute("SELECT * FROM Scraper WHERE (`CHANNEL` = 'nexta_tv' OR `CHANNEL` = 'nexta_live') AND NOT (`XPost` = 't.me/nexta_tv' OR `XPost` = 't.me/nexta_live' OR `XPost` = 't.me/luxta_tv') AND (`5.5 Malintent` = 1)").fetchall()
+        #set_5 = db.execute("SELECT * FROM Scraper WHERE (`CHANNEL` = 'nexta_tv' OR `CHANNEL` = 'nexta_live') AND NOT (`XPost` = 't.me/nexta_tv' OR `XPost` = 't.me/nexta_live' OR `XPost` = 't.me/luxta_tv') AND (`5.6 Illness` = 1)").fetchall()
+        #set_6 = db.execute("SELECT * FROM Scraper WHERE (`CHANNEL` = 'nexta_tv' OR `CHANNEL` = 'nexta_live') AND NOT (`XPost` = 't.me/nexta_tv' OR `XPost` = 't.me/nexta_live' OR `XPost` = 't.me/luxta_tv') AND (`5.7 Stupidity` = 1)").fetchall()
+        #set_7 = db.execute("SELECT * FROM Scraper WHERE (`CHANNEL` = 'nexta_tv' OR `CHANNEL` = 'nexta_live') AND NOT (`XPost` = 't.me/nexta_tv' OR `XPost` = 't.me/nexta_live' OR `XPost` = 't.me/luxta_tv') AND (`5.8 Namecalling` = 1)").fetchall()
+        #set_8 = db.execute("SELECT * FROM Scraper WHERE (`CHANNEL` = 'nexta_tv' OR `CHANNEL` = 'nexta_live') AND NOT (`XPost` = 't.me/nexta_tv' OR `XPost` = 't.me/nexta_live' OR `XPost` = 't.me/luxta_tv') AND (`5.9 Dehumanization` = 1)").fetchall()
+        #set_9 = db.execute("SELECT * FROM Scraper WHERE (`CHANNEL` = 'nexta_tv' OR `CHANNEL` = 'nexta_live') AND NOT (`XPost` = 't.me/nexta_tv' OR `XPost` = 't.me/nexta_live' OR `XPost` = 't.me/luxta_tv') AND (`5.10 Cowardice` = 1)").fetchall()
 
     # get what days posts were on
     set_1_days = [post[4][:10] for post in set_1]
     set_2_days = [post[4][:10] for post in set_2]
+    #set_3_days = [post[4][:10] for post in set_3]
+    #set_4_days = [post[4][:10] for post in set_4]
+    #set_5_days = [post[4][:10] for post in set_5]
+    #set_6_days = [post[4][:10] for post in set_6]
+    #set_7_days = [post[4][:10] for post in set_7]
+    #set_8_days = [post[4][:10] for post in set_8]
+    #set_9_days = [post[4][:10] for post in set_9]
 
     # get days for all the messages pulled
     start = datetime(year = 2020, month = 8, day = 1)
@@ -38,15 +52,29 @@ def plot():
     # get number of posts per day
     set_1_cnts = [set_1_days.count(date.isoformat()[:10]) for date in days]
     set_2_cnts = [set_2_days.count(date.isoformat()[:10]) for date in days]
+    #set_3_cnts = [set_3_days.count(date.isoformat()[:10]) for date in days]
+    #set_4_cnts = [set_4_days.count(date.isoformat()[:10]) for date in days]
+    #set_5_cnts = [set_5_days.count(date.isoformat()[:10]) for date in days]
+    #set_6_cnts = [set_6_days.count(date.isoformat()[:10]) for date in days]
+    #set_7_cnts = [set_7_days.count(date.isoformat()[:10]) for date in days]
+    #set_8_cnts = [set_8_days.count(date.isoformat()[:10]) for date in days]
+    #set_9_cnts = [set_9_days.count(date.isoformat()[:10]) for date in days]
 
     # make subfigure to have more control over axes
     fig, ax = plt.subplots()
 
     # add title
-    plt.title("Posts referencing the elderly compared to posts referencing WWII")
+    plt.title("A plot")
     # plot data
-    plt.plot(days, set_1_cnts, label = "Elderly")
-    plt.plot(days, set_2_cnts, label = "WWII")
+    plt.plot(days, set_1_cnts, label = "Violent tactics")
+    plt.plot(days, set_2_cnts, label = "Dehumanization")
+    #plt.plot(days, set_3_cnts, label = "Incompetence")
+    #plt.plot(days, set_4_cnts, label = "Malintent")
+    #plt.plot(days, set_5_cnts, label = "Illness")
+    #plt.plot(days, set_6_cnts, label = "Stupidity")
+    #plt.plot(days, set_7_cnts, label = "Namecalling")
+    #plt.plot(days, set_8_cnts, label = "Dehumanization")
+    #plt.plot(days, set_9_cnts, label = "Cowardice")
 
     # format dates
     ax.xaxis.set_major_formatter(DateFormatter("%m-%d"))
